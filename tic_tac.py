1661749df602be098db03af00b85df1c92f6edbd
Alberto <a.diana.dev@gmail.com>
2020-10-28 19:54:10 +0100

    Added ai logic and improved main loop

--- tic_tac.py
+++ tic_tac.py
@@ -3,10 +3,15 @@ A tic tac toe game written in python
 """
 import random
 import platform
+import math
 import os
 
 PLAYER = -1
 CPU = 1
+INFINITY = math.inf
+PLAYERTOKEN = ""
+CPUTOKEN = ""
+EXIT = False
 board = [
     [0,0,0],
     [0,0,0],
@@ -15,8 +20,8 @@ board = [
 
 #Functions
 
-def print_board(matrix):
-    """ 
+def print_board(matrix, p_token, cpu_token):
+    """
     Prints the current state of the board
     """
     game_board = ""
@@ -24,7 +29,7 @@ def print_board(matrix):
         if i == 0:
             game_board += " A   B   C \n \n"
         for j, cell in enumerate(row):
-            placeholder = get_placeholder(cell)
+            placeholder = get_placeholder(cell, p_token, cpu_token)
             if j < len(row) - 1:
                 text = " {element} |"
                 game_board += text.format(element = placeholder)
@@ -35,14 +40,14 @@ def print_board(matrix):
             game_board += ("---+---+---\n")
     print(game_board)
 
-def get_placeholder(cell):
+def get_placeholder(cell, p_token, cpu_token):
     """
     Returns appropriate placeholder based on the cell value
     """
     if cell == -1:
-        placeholder = "X"
+        placeholder = p_token
     elif  cell == 1:
-        placeholder = "O"
+        placeholder = cpu_token
     else:
         placeholder = " "
     return placeholder
@@ -58,7 +63,7 @@ def clean():
         os.system('clear')
 
 def get_winner(matrix):
-    """ 
+    """
     Checks the state of the board to see i a player has won
     """
     #check first diagonal
@@ -90,15 +95,43 @@ def get_winner(matrix):
             return cell
     return 0
 
+def minimax(board_state, depth, player):
+    """
+    Function that finds the optimal move for the specified player
+    """
+    if player == CPU:
+        best = [-1, -1, -INFINITY]
+    else:
+        best =[-1, -1, INFINITY]
+
+    if depth == 0 or get_winner(board) != 0:
+        score = get_winner(board_state)
+        return[-1,-1, score]
+
+    for cell in get_free_cells(board_state):
+        x_index ,y_index  = cell[0], cell[1]
+        board_state = change_board_state(x_index, y_index, player, board_state)
+        score = minimax(board_state, depth - 1, -player)
+        board_state = change_board_state(x_index, y_index, 0, board_state)
+        score[0], score[1] = x_index, y_index
+
+        if player == CPU:
+            if score[2] > best[2]:
+                best = score
+        else:
+            if score[2] < best[2]:
+                best = score
+    return best
+
 def get_move():
     """
     Gets human player input
     """
     print("Insert column letter")
-    column = input()
+    column = input().strip()
     while not validate_columns(column):
         print("Invalid input, insert column letter")
-        column = input()
+        column = input().strip()
     if column == "a":
         column_index = 0
     elif column == "b":
@@ -106,11 +139,24 @@ def get_move():
     elif column == "c":
         column_index = 2
     print("Insert row number")
-    row_index = input()
+    row_index = input().strip()
     while not validate_rows(row_index):
         print("Invalid input, insert row number")
-        row_index = input()
+        row_index = input().strip()
     return (int(row_index), column_index)
+
+def cpu_move(board_state):
+    """
+    Functions that uses the minimax function to find the optimal move for the cpu
+    """
+    depth = len(get_free_cells(board_state))
+    if depth == 0 or get_winner(board) != 0:
+        return
+    if depth == 9:
+        return random.choice(get_free_cells(board))
+    else:
+        move = minimax(board_state, depth, CPU)
+        return move
 
 def validate_columns(column_id):
     """
@@ -138,11 +184,18 @@ def validate_rows(row_id):
         validated= False
     return validated
 
-def set_board_cell(x_move, y_move, player):
+def validate_choice(choice):
+    """
+    Validates player token choice
+    """
+    return choice.isalpha() and (choice.upper() == "X" or choice.upper() == "O")
+
+def change_board_state(x_move, y_move, player, board_state):
     """
     Sets the selected cell for the player or the cpu
     """
-    board[x_move][y_move] = player
+    board_state[x_move][y_move] = player
+    return board_state
 
 def get_free_cells(matrix):
     """
@@ -155,15 +208,35 @@ def get_free_cells(matrix):
                 free_cells.append((i,j))
     return free_cells
 
-while get_winner(board) != -1:
-    #player turn
-    print("Player turn (X)")
-    print_board(board)
-    move_player = get_move()
-    set_board_cell(move_player[0], move_player[1], PLAYER)
-    #cpu turn
-    print("CPU turn (O)")
-    print_board(board)
-    move_CPU = random.choice(get_free_cells(board))
-    set_board_cell(move_CPU[0], move_CPU[1], CPU)
-    clean()
+#Main loop
+print("Type X or O to chose your tokens, X always goes first")
+player_choice = input().strip()
+while not EXIT:
+    
+    while validate_choice(player_choice):
+        print("Invalid token, please select X or O")
+        player_choice = input().strip()
+    if player_choice.upper() == "X":
+        PLAYERTOKEN = "X"
+        CPUTOKEN = "O"
+    else:
+        PLAYERTOKEN = "O"
+        CPUTOKEN = "X"
+
+    while len(get_free_cells(board)) !=0 and get_winner(board) == 0:
+        #player turn
+        print("Player turn (X)")
+        print_board(board, PLAYERTOKEN, CPUTOKEN)
+        move_player = get_move()
+        board = change_board_state(move_player[0], move_player[1], PLAYER, board)
+        #cpu turn
+        move_CPU = cpu_move(board)
+        board = change_board_state(move_CPU[0], move_CPU[1], CPU, board)
+        clean()
+
+    if get_winner(board) == 1:
+        print("CPU wins")
+    elif get_winner(board) == 1:
+        print("You win")
+    else:
+        print("It's a draw")    
